c9a322b1d3897be305e0b4d0168b3ae3503b0f5c
byron <byroncoughlinsr@gmail.com>
2021-04-23 12:36:48 -0700

    Changes to sequelize file

--- qs-server/src/sequelize.js
+++ qs-server/src/sequelize.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres');
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DATABASE_PASSWORD;
+  const dbHost = process.env.DB_HOST;
+  const dbPort = process.env.DB_PORT;
+  const dbDatabase = process.env.DB_DATABASE;
+  const connectionString = 'postgresql://' + dbUser + ':' + dbPassword + '@' + dbHost + ':' + dbPort + '/' + dbDatabase;
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
     logging: false,
