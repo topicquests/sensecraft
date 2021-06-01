8f1fd7322405b51da40a667c69a6cf3af9d87853
Jack Park <jackpark@gmail.com>
2021-06-01 11:06:14 -0700

    Starting Quest state git push ;

---
+++ qs-client/src/store/modules/quest/actions.js
@@ -0,0 +1,20 @@
+import userService from "../../../services/user";
+import { Notify } from "quasar";
+
+
+export async function registerUser({commit, rootState}, payload) {
+  let result = await userService
+    .signup(
+      payload.formdata.firstname,
+      payload.formdata.lastname,
+      payload.formdata.email,
+      payload.formdata.password,
+      payload.formdata.handle
+    )      
+};
+
+
+
+
+
+
