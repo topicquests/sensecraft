26ca581136d710bfc14311179f4270174acad214
Byron Coughlin <byroncoughlinsr@gmail.com>
2021-06-22 08:56:36 -0700

    Add lobby page

---
+++ qs-client/src/pages/Lobby.vue
@@ -0,0 +1,28 @@
+<template>
+   <q-page :padding="true" class = "flex flex-center">
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api'
+export default {
+  data () 
+  { return 
+
+  },
+
+  computed: {
+
+  },
+
+  method: {
+
+  },
+
+  mounted () {
+
+  }
+  // name: 'PageName',
+}
+</script>
