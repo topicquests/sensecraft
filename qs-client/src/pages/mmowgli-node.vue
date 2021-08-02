a82a49604440c44a40e9debcd3fbba9ea0328f88
Jack Park <jackpark@gmail.com>
2021-08-02 16:13:57 -0700

    Cleaned up mmowgli-node, removed unneeded templates

--- qs-client/src/pages/mmowgli-node.vue
+++ qs-client/src/pages/mmowgli-node.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-page :padding="true">
+  <q-page >
     <!-- Create a card centered in an upper row -->
-    <q-card inline style="width: 500px margin: auto;
+    <div inline style="width: 500px margin: auto;
       width: 60%;">
       <!-- guessing on label -->
-      <q-card-title>{{ label }}</q-card-title>
+      <h4><img style="margin-right:4px;" :src="q.img" >{{ q.label }}</h4>
       <!-- todo
         needs
           an ibis icon - how to bring that in, depends on
@@ -13,133 +13,51 @@
           context - passed in allows to know whether transcluded
           maybe other things
           -->
-    </q-card>
-<!-- What follows is any child nodes
-   and tags around this topic
-   tags dragged here from FeatherWeight
-   they will be removed for the demo, but returned later -->
-    <q-list>
-      <q-collapsible image="statics/images/issue.png" label="Questions" >
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/nodeedit/question/${q.type}/${q.nodeId}`">
-              New Question</a>
-          </div>
-          <q-list>
-            <q-item class="node wordwrap" v-for="question in q.questions" :key="question.nodeId">
-              <router-link :to="{ name: 'questview', params: { id: question.nodeId }}">{{ question.label }}</router-link>
-            </q-item>
-          </q-list>
+    </div>
+    <div class="details">
+      <div  v-html="q.details"></div>
+    </div>
+    <div class="columnscroller">
+      <div class="columncontainer">
+        <div class="column headerNode" style="text-align: center;">
+          <img class="headerimage" src="icons/ibis/issue_sm.png">Question
+          <a v-if="isAuthenticated" title="Respond" :href="'/conversation/newquestion/' + q.nodeId"><img class="respond" src="icons/respond_sm.png"></a>
         </div>
-      </q-collapsible>
-      <q-collapsible image="statics/images/position.png" label="Answers"      >
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/nodeedit/answer/${q.type}/${q.nodeId}`">
-              New Answer</a>
-          </div>
+        <div class="column headerNode" style="text-align: center;">
+          <img class="headerimage" src="icons/ibis/position_sm.png">Answer
+          <a v-if="isAuthenticated" :href="'/conversation/newanswer/'+ q.nodeId"><img class="respond" src="icons/respond_sm.png"></a>
         </div>
-        <q-list>
-          <q-item class="node" v-for="answer in q.answers" :key="answer.nodeId">
-            <router-link :to="{ name: 'questview', params: { id: answer.nodeId }}">{{ answer.label }}</router-link>
-          </q-item>
-        </q-list>
-      </q-collapsible>
-      <q-collapsible image="statics/images/plus.png" label="Pro Arguments">
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/nodeedit/pro/${q.type}/${q.nodeId}`">
-              New Pro Argument</a>
-          </div>
+        <div class="column headerNode" style="text-align: center;">
+          <img class="headerimage" src="icons/ibis/plus_sm.png">Pro
+          <a v-if="isAuthenticated" :href="'/conversation/newpro/' + q.nodeId"><img class="respond" src="icons/respond_sm.png"></a>
         </div>
-        <q-list>
-          <q-item class="node" v-for="pro in q.pros" :key="pro.nodeId">
-            <router-link :to="{ name: 'questview', params: { id: pro.nodeId }}">{{ pro.label }}</router-link>
-          </q-item>
-        </q-list>
-      </q-collapsible>
-      <q-collapsible image="statics/images/minus.png" label="Con Arguments">
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/nodeedit/con/${q.type}/${q.nodeId}`">
-              New Con Argument</a>
-          </div>
+        <div class="column headerNode" style="text-align: center;">
+          <img class="headerimage" src="icons/ibis/minus_sm.png">Con
+          <a v-if="isAuthenticated" :href="'/conversation/newcon/' +q.nodeId"><img class="respond" src="icons/respond_sm.png"></a>
         </div>
-        <q-list>
-          <q-item class="node" v-for="con in q.cons" :key="con.nodeId">
-            <router-link :to="{ name: 'questview', params: { id: con.nodeId }}">{{ con.label }}</router-link>
-          </q-item>
-        </q-list>
-      </q-collapsible>
-      <q-collapsible v-if="isTopic" image="statics/images/cogwheel.png" label="Subclasses">
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/topicchild/${q.nodeId}/subclass`">
-              New Subclass Topic</a>
-          </div>
+        <div class="column headerNode" style="text-align: center;">
+          <img class="headerimage" src="icons/ibis/reference_sm.png">Refs
+          <a v-if="isAuthenticated" :href="'/conversation/newref/' +q.nodeId"><img class="respond" src="icons/respond_sm.png"></a>
         </div>
-        <q-list>
-          <q-item class="node" v-for="sub in q.subclasses" :key="sub.nodeId">
-            <router-link :to="{ name: 'topicview', params: { id: sub.nodeId }}">{{ sub.label }}</router-link>
-          </q-item>
-        </q-list>
-      </q-collapsible>
-      <q-collapsible v-if="isTopic" image="statics/images/cogwheel.png" label="Instances">
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/topicchild/${q.nodeId}/instance`">
-              New Instance Topic</a>
-          </div>
-        <q-list>
-          <q-item class="node" v-for="inst in q.instances" :key="inst.nodeId">
-            <router-link :to="{ name: 'topicview', params: { id: inst.nodeId }}">{{ inst.label }}</router-link>
-          </q-item>
-        </q-list>
+      </div> <!-- end column headers -->
+      <div class="datacontainer"> <!-- start column data -->
+        <div class="datacolumn node" v-for="question in q.questions" :key="question.nodeId">
+          <router-link :to="{ name: 'node', params: { id: question.nodeId, context: '' }}">{{ question.label }}</router-link>
         </div>
-      </q-collapsible>
-      <q-collapsible image="statics/images/tag.png" label="Tags">
-        <div>
-          <div v-if="isAuthenticated" class="node">
-            <a :href="`/tagform/${q.nodeId}`">
-              Add Tags</a>
-          </div>
+        <div class="datacolum node" v-for="answer in q.answers" :key="answer.nodeId">
+          <router-link :to="{ name: 'node', params: { id: answer.nodeId, context: ''  }}">{{ answer.label }}</router-link>
         </div>
-        <q-list>
-          <q-item class="node" v-for="tag in q.tags" :key="tag.nodeId">
-            <router-link :to="{ name: 'tagview', params: { id: tag.nodeId }}">{{ tag.label }}</router-link>
-          </q-item>
-        </q-list>
-      </q-collapsible>
-      <q-collapsible image="statics/images/link.png" label="Connections">
-        <div>
-          <div>
-            <div v-if="isAuthenticated" class="node">
-              <a :href="`/connedit/${q.nodeId}`">
-                New Connection</a>
-            </div>
-          </div>
-          <q-list>
-            <q-item class="node" v-for="reln in q.relations" :key="reln.nodeId">
-              <router-link :to="{ name: 'topicview', params: { id: reln.nodeId}}">{{ reln.label }}</router-link>
-            </q-item>
-          </q-list>
+        <div class="datacolumn node" v-for="pro in q.pros" :key="pro.nodeId">
+          <router-link :to="{ name: 'node', params: { id: pro.nodeId, context: ''  }}">{{ pro.label }}</router-link>
         </div>
-      </q-collapsible>
-      <q-collapsible image="statics/images/properties.png" label="Properties">
-        <div>
-          <div>
-            <div v-if="isAuthenticated" class="node">
-              <a :href="`/props/${q.nodeId}`">
-                New Property</a>
-            </div>
-          </div>
-          <q-list>
-<q-item class="node" v-for="{value, key} in q.properties" :key="key">
-             <b>Key:</b> {{key}} &nbsp; <b>Value:</b> {{cleanup(value)}}
-   </q-item>          </q-list>
+        <div class="datacolumn node" v-for="con in q.cons" :key="con.nodeId">
+          <router-link :to="{ name: 'node', params: { id: con.nodeId, context: ''  }}">{{ con.label }}</router-link>
         </div>
-      </q-collapsible>
-    </q-list>
+        <div class="datacolumn node" v-for="ref in q.refs" :key="ref.nodeId">
+          <router-link :to="{ name: 'node', params: { id: ref.nodeId, context: ''  }}">{{ ref.label }}</router-link>
+        </div>
+      </div> <!-- edd colum data -->
+    </div>
   </q-page>
 </template>
 
@@ -153,18 +71,68 @@
     props: ["id", "context"],
     data () {
       return {
+        label: '',
+        q: ''
 
-        label: ''
-        //details: '',
-        //url: '',
-        // id: '',
-        // member:
       }
     },
     computed: {
+      canEdit () {
+        return false;
+        //TODO
+      },
       isAuthenticated () {
-        return this.$store.getters.isAuthenticated
+        return true;
+        //TODO return this.$store.getters.isAuthenticated
       }
+    },
+    mounted () {
+      this.nodeId = this.$route.params.id
+      //this.$data.rightDrawerOpen = false
+      //turn off conversation tree
+      //this.$store.commit('questView', false)
+
+
+      this.initialize(this.nodeId)
+    },
+    methods: {
+
+      async initialize (id = null) {
+        //this.$store.commit('questView', true)
+        const nodeId = id || this.$route.params.id
+        console.info('Initialize', 'fetching data for ', nodeId)
+        this.q = this.mock(); //this.$store.quest.getters.getNode(); //('foo')
+        console.info('node', this.q)
+        if (this.q !== null) {
+          //this.label = q.label;
+          //add image to q
+          this.getImage();
+        } else {
+          this.label = 'Bad'
+        }
+      },
+
+
+      getImage () {
+        const type = this.q.type;
+        const rslt = 'icons/ibis/issue.png';
+        console.log('ICON', type, rslt);
+        this.q.img = rslt;
+      },
+      mock () {
+        const node = {};
+        node.nodeId= 'foo',
+        node.label= 'Why is the sky blue?',
+        node.type= 'Question',
+        node.details= "Now is the time for all good men to come to the aid of their country.",
+        node.questions= [],
+        node.answers= [];
+        node.pro= [],
+        node.cons=[],
+        node.refs= []
+        return node;
+      }
+
     }
   }
 
@@ -172,8 +140,8 @@
 </script>
 <style lang="styl">
 .q-item-image {
-    min-width: 20px;
-    max-width: 20px;
+    min-width: 10px;
+    max-width: 10px;
 }
 
 .scroll.relative-position.overflow-hidden.fit.q-touch {
@@ -226,5 +194,74 @@
   margin: 12px;
   font-family: pragmatica-web, sans-serif;
   border-radius: 3px;
+}
+
+/** from view.hbs */
+/**
+ * Enable columns to scroll right and left
+ */
+.columnscroller {
+    border: 1px solid black;
+    width: 100%;
+    white-space:nowrap;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    margin: 0px;
+    border-radius: 3px;
+}
+
+/**
+ * width is set to accomodate lots of columns.
+ * If they wrap when adding more columns, then
+ * width must increase.
+ * The formula seems to be column width * num colums + 100px  2500
+ */
+.columncontainer {
+	width: 1200px;
+}
+.datacontainer {
+    width: 1200px;
+}
+
+.headerimage {
+    vertical-align: middle;
+    margin-right: 4px;
+}
+
+.headernode {
+  float: top;
+  border: 1px solid black;
+  border-radius: 3px;
+  font-family: pragmatica-web, sans-serif;
+  white-space: -moz-pre-wrap !important; /* Mozilla, since 1999 */
+  white-space: -pre-wrap; /* Opera 4-6 */
+  white-space: -o-pre-wrap; /* Opera 7 */
+  white-space: pre-wrap; /* css-3 */
+  word-wrap: break-word; /* Internet Explorer 5.5+ */
+  white-space: normal;
+}
+.column {
+    float:left;
+    white-space:normal;
+    border: 0px solid black;
+    width: 260px;
+    height: 34px;
+    background: rgb(240, 234, 234);
+    border-radius: 3px;
+    margin-left: 1px;
+    margin-right: 1px;
+    font-family:pragmatica-web,sans-serif;
+}
+.datacolumn {
+    height: 400px;
+    float:left;
+    border: 1px solid white;
+    width: 270px;
+    background: white;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    break-inside: avoid;
+    margin-left: 4px;
+    margin-right: 4px;
 }
 </style>
