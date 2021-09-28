ca45a25e206a7dbeaa1134fd7e14808b969184d3
Marc-Antoine Parent <maparent@acm.org>
2021-09-27 21:32:25 -0400

    disalow debugger in eslint

--- qs-client/.eslintrc.js
+++ qs-client/.eslintrc.js
@@ -57,9 +57,9 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    "prefer-promise-reject-errors": "off",
+    "prefer-promise-reject-errors": "warn",
 
     // allow debugger during development only
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": "error",
   },
 };
