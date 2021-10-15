ed4a8f4e89a69ef85772b6963f3529909c39e788
Marc-Antoine Parent <maparent@acm.org>
2021-10-14 23:25:00 -0400

    color-code node status

--- qs-client/src/components/node-tree.vue
+++ qs-client/src/components/node-tree.vue
@@ -4,7 +4,16 @@
     node-key="id"
     default-expand-all="true"
     :selected.sync="selectedNodeId"
-  />
+  >
+    <template v-slot:default-header="prop">
+      <div class="row items-center">
+        <q-icon :name="prop.node.icon" class="q-mr-sm" />
+        <div :class="'node-status-' + prop.node.data.status">
+          {{ prop.node.label }}
+        </div>
+      </div>
+    </template>
+  </q-tree>
 </template>
 
 <script lang="ts">
@@ -35,3 +44,23 @@ export default class NodeTree extends NodeTreeProps {
   }
 }
 </script>
+<style>
+.node-status-private_draft {
+  color: red;
+}
+.node-status-proposed {
+  color: green;
+}
+.node-status-guild_draft {
+  color: orange;
+}
+.node-status-published {
+  color: black;
+}
+.node-status-submitted {
+  color: purple;
+}
+.node-status-obsolete {
+  color: grey;
+}
+</style>
