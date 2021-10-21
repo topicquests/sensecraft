d1c9108786ce929a2099b45398f88ee8c9139c84
Byron Coughlin <byroncoughlinsr@gmail.com>
2021-10-21 14:46:37 -0700

    role permissions

--- qs-client/src/enums.ts
+++ qs-client/src/enums.ts
@@ -76,6 +76,7 @@ export const ibis_node_type_list: ibis_node_type_type[] = Object.keys(
 export enum publication_state_enum {
   obsolete = "obsolete",
   private_draft = "private_draft",
+  role_draft = "role_draft",
   guild_draft = "guild_draft",
   proposed = "proposed",
   submitted = "submitted",
@@ -86,6 +87,21 @@ export type publication_state_type = keyof typeof publication_state_enum;
 export const publication_state_list: publication_state_type[] = Object.keys(
   publication_state_enum
 ) as publication_state_type[];
+
+export enum meta_type_enum {
+  obsolete = "obsolete",
+  private_draft = "private_draft",
+  role_draft = "role_draft",
+  guild_draft = "guild_draft",
+  proposed = "proposed",
+  submitted = "submitted",
+  published = "published",
+}
+
+export type meta_type_type = keyof typeof meta_type_enum;
+export const meta_type_list: meta_type_type[] = Object.keys(
+  meta_type_enum
+) as meta_type_type[];
 
 export const public_private_bool = [
   {
