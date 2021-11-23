0176829b53c68e79389ae6cef8cb3069a85d1a5b
Marc-Antoine Parent <maparent@acm.org>
2021-11-23 14:13:58 -0500

    typo

--- qs-server/deploy/casting-2M.sql
+++ qs-server/deploy/casting-2M.sql
@@ -5,6 +5,6 @@ BEGIN;
 
 ALTER TABLE public.casting
   ADD CONSTRAINT casting_game_play_fkey FOREIGN KEY (quest_id, guild_id)
-      REFERENCES public.game_play(quest_id, guild_id) ON UPDATE CASCADE ON DELETE CASCADE
+      REFERENCES public.game_play(quest_id, guild_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 COMMIT;
