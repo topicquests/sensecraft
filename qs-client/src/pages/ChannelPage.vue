988bc079ea02cdbb681469dc0e5558e2d7dd7dfb
Jack Park <jackpark@gmail.com>
2021-12-07 14:15:08 -0800

    adding code

--- qs-client/src/pages/ChannelPage.vue
+++ qs-client/src/pages/ChannelPage.vue
@@ -22,5 +22,60 @@
     </div>
   </q-page>
 </template>
-<script></script>
+<script>
+import { mapGetters, mapActions, mapState } from "vuex";
+import scoreboard from "../components/scoreboard.vue";
+import member from "../components/member.vue";
+import questCard from "../components/quest-card.vue";
+import nodeCard from "../components/node-card.vue";
+import nodeForm from "../components/node-form.vue";
+import nodeTree from "../components/node-tree.vue";
+import {
+  ibis_node_type_enum,
+  ibis_node_type_type,
+  ibis_node_type_list,
+  publication_state_list,
+  public_private_bool,
+} from "../enums";
+import app from "../App.vue";
+import {
+  ConversationState,
+  ConversationGetterTypes,
+  ConversationActionTypes,
+  ibis_child_types,
+} from "../store/conversation";
+import {
+  QuestsState,
+  QuestsActionTypes,
+  QuestsGetterTypes,
+} from "../store/quests";
+import {
+  GuildsState,
+  GuildsGetterTypes,
+  GuildsActionTypes,
+} from "../store/guilds";
+import {
+  MemberState,
+  MemberGetterTypes,
+  MemberActionTypes,
+} from "../store/member";
+import {
+  registration_status_enum,
+  quest_status_enum,
+  permission_enum,
+} from "../enums";
+import {
+  Quest,
+  Guild,
+  GamePlay,
+  Casting,
+  ConversationNode,
+  Member,
+} from "../types";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { MembersGetterTypes, MembersActionTypes } from "../store/members";
+import { BaseGetterTypes } from "../store/baseStore";
+</script>
+
 <style></style>
