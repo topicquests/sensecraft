d63ccd503dd83a1d5e6300462084936d806d184f
Byron Coughlin <byroncoughlinsr@gmail.com>
2021-12-18 07:32:36 -0800

    Progress on adding imput tests for components mainlayout

--- qs-client/src/layouts/__tests__/MainLayout.spec.js
+++ qs-client/src/layouts/__tests__/MainLayout.spec.js
@@ -21,15 +21,16 @@ import router from "../../router";
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
+var store;
+var storeOptions;
+var wrapper;
 
 import MainLayout from "../MainLayout";
 
 // reset spies, initial state and getters
-//afterEach(() => store.reset());
+afterEach(() => store.reset());
 
-describe("MainLayout.vue", () => {
-  let wrapper;
-  let storeOptions;
+describe("MainLayout.vue not logged in", () => {
   beforeEach(() => {
     storeOptions = {
       state: {
@@ -42,12 +43,12 @@ describe("MainLayout.vue", () => {
         conversation: {},
       },
       getters: {
-        hasPermission: () => true,
+        hasPermission: () => false,
         "conversation/getNeighbourhoodTree": () => [],
       },
     };
     // create the Store mock
-    const store = new Store(storeOptions);
+    store = new Store(storeOptions);
     // add other mocks here so they are accessible in every component
     const mocks = {
       $store: store,
@@ -81,7 +82,7 @@ describe("MainLayout.vue", () => {
     expect(wrapper).toBeTruthy();
   });
 
-  //afterEach(() => store.reset());
+  afterEach(() => store.reset());
 
   //Home image button
   it("renders home image button", () => {
@@ -96,8 +97,8 @@ describe("MainLayout.vue", () => {
   //Signup Button
   it("renders signup button", () => {
     expect(wrapper.find("#register").exists()).toBe(true);
-
-    //Member not logged in
+  });
+  it("Signup button visible if no user authenticated", () => {
     expect(wrapper.find("#register").isVisible()).toBe(true);
   });
 
@@ -105,31 +106,23 @@ describe("MainLayout.vue", () => {
   it("renders signin button", () => {
     expect(wrapper.find("#signin").exists()).toBe(true);
   });
-
   it("Signin button is visible if no user authenticated", () => {
     expect(wrapper.find("#signin").isVisible()).toBe(true);
   });
-
-  it("logout button is shown if user authenticated", () => {});
 
   //Logoff Button
   it("renders logout button", () => {
     expect(wrapper.find("#logoff").exists()).toBe(true);
   });
-
-  it("Logoff buttun is not visible if no user logged on", () => {
+  it("Logoff buttun is not visible no user authenticated", () => {
     expect(wrapper.find("#logoff").isVisible()).toBe(false);
   });
-
   it("logs out", async () => {
     const button = wrapper.find("#logoff");
     button.vm.$q.notify = jest.fn();
     await button.vm.$emit("click");
-    // expect(store.dispatch).toHaveBeenCalledWith("member/logout");
+    expect(store.dispatch).toHaveBeenCalledWith("member/logout");
   });
-
-  it("logout button is hid if no user authenticated", () => {});
-  it("logout button is shown if user authenticated", () => {});
 
   //Conversation node tree Button
   it("renders conversation node tree button", () => {
@@ -140,34 +133,165 @@ describe("MainLayout.vue", () => {
   it("renders menu item lobby", () => {
     expect(wrapper.find("#lobby").exists()).toBe(true);
   });
+  it("Lobby buttun not visible no user authenticated", () => {
+    expect(wrapper.find("#lobby").isVisible()).toBe(false);
+  });
 
   //Menu item quest view
   it("renders menu item quest view", () => {
     expect(wrapper.find("#questView").exists()).toBe(true);
+  });
+  it("quest view buttun is always visible", () => {
+    expect(wrapper.find("#questView").isVisible()).toBe(true);
   });
 
   //Menu item create quest
   it("renders menu item create quest", () => {
     expect(wrapper.find("#createQuest").exists()).toBe(true);
   });
+  it("quest create buttun is not visible no permission", () => {
+    expect(wrapper.find("#createQuest").isVisible()).toBe(false);
+  });
 
   //Menu item guild list
   it("renders menu item guild list", () => {
     expect(wrapper.find("#guildView").exists()).toBe(true);
+  });
+  it("guild view buttun is always visible", () => {
+    expect(wrapper.find("#guildView").isVisible()).toBe(true);
   });
 
   //Menu item create guild
   it("renders menu item create guild", () => {
     expect(wrapper.find("#createGuild").exists()).toBe(true);
   });
+  it("quild create buttun is not visible no permission", () => {
+    expect(wrapper.find("#createGuild").isVisible()).toBe(false);
+  });
 
   //Menu item home
   it("renders menu item home", () => {
     expect(wrapper.find("#home").exists()).toBe(true);
   });
+  it("home buttun is always visible", () => {
+    expect(wrapper.find("#home").isVisible()).toBe(true);
+  });
 
   //Menu item admin
   it("renders menu item admin", () => {
     expect(wrapper.find("#admin").exists()).toBe(true);
+  });
+  it("admin link is not visible no permission", () => {
+    expect(wrapper.find("#admin").isVisible()).toBe(false);
+  });
+});
+
+describe("MainLayout.logged in", () => {
+  beforeEach(() => {
+    storeOptions = {
+      state: {
+        member: {
+          isAuthenticated: true,
+          member: {
+            id: 1,
+          },
+        },
+        conversation: {},
+      },
+      getters: {
+        hasPermission: () => true,
+        "conversation/getNeighbourhoodTree": () => [],
+      },
+    };
+    // create the Store mock
+    store = new Store(storeOptions);
+    // add other mocks here so they are accessible in every component
+    const mocks = {
+      $store: store,
+    };
+
+    wrapper = mountQuasar(MainLayout, {
+      mount: {
+        mocks,
+        localVue,
+        router: router(),
+      },
+      quasar: {
+        components: {
+          QBtn,
+          QHeader,
+          QLayout,
+          QToolbar,
+          QToolbarTitle,
+          QImg,
+          QDrawer,
+          QScrollArea,
+          QList,
+          QItem,
+          QPageContainer,
+          QFooter,
+          QIcon,
+        },
+      },
+      propsData: {},
+    });
+    expect(wrapper).toBeTruthy();
+  });
+
+  afterEach(() => store.reset());
+
+  //Signup Button
+  it("Signup button not visible if user authenticated", () => {
+    expect(wrapper.find("#register").isVisible()).toBe(false);
+  });
+
+  //Signin Button
+  it("Signin button not visible if user authenticated", () => {
+    expect(wrapper.find("#signin").isVisible()).toBe(false);
+  });
+
+  //Logoff Button
+  it("Logoff buttun is visible if user authenticated", () => {
+    expect(wrapper.find("#logoff").isVisible()).toBe(true);
+  });
+
+  //Conversation node tree Button
+  it("renders conversation node tree button", () => {
+    expect(wrapper.find("#conversation_tree").exists()).toBe(true);
+  });
+
+  //Menu item lobby
+  it("Lobby buttun visible if user authenticated", () => {
+    expect(wrapper.find("#lobby").isVisible()).toBe(true);
+  });
+
+  //Menu item quest view
+  it("quest view buttun is always visible", () => {
+    expect(wrapper.find("#questView").isVisible()).toBe(true);
+  });
+
+  //Menu item create quest
+  it("quest create buttun is visible user has permission", () => {
+    expect(wrapper.find("#createQuest").isVisible()).toBe(true);
+  });
+
+  //Menu item guild list
+  it("guild view buttun is always visible", () => {
+    expect(wrapper.find("#guildView").isVisible()).toBe(true);
+  });
+
+  //Menu item create guild
+  it("quild create link is visible user has permission", () => {
+    expect(wrapper.find("#createGuild").isVisible()).toBe(true);
+  });
+
+  //Menu item home
+  it("home buttun is always visible", () => {
+    expect(wrapper.find("#home").isVisible()).toBe(true);
+  });
+
+  //Menu item admin
+  it("admin link is visible user has permission", () => {
+    expect(wrapper.find("#admin").isVisible()).toBe(true);
   });
 });
