f5d5fe4312c1e6dec22c5c9139ca455c12b34db0
Marc-Antoine Parent <maparent@acm.org>
2022-02-04 11:37:12 -0500

    simplify

--- qs-client/src/pages/HouseRules.vue
+++ qs-client/src/pages/HouseRules.vue
@@ -1,21 +1,17 @@
 <template>
   <q-page>
     <h2>House Rules</h2>
-    <h4>about</h4>
-    Sensecraft promotes an ecosystem of <b>Trust</b> and <b>Safety</b>. To
-    support that, we enumerate below a minimal set of rules which govern the
-    determination of game move behaviors which will be deemed unacceptable.<br /><br />
-    These are simple rules, a minimalist set. We expect to improve on them by
-    way of public Quests to gain deeper insight into the nature of game
-    governance.
-    <br />
-    <h4>Rules</h4>
-    We list the categories of behaviors, as exhibited in game moves, which are
-    strictly forbidden.<br />
-    <h6>Personal attack</h6>
-    Including Slander, Libel, ad hominem attacks
-    <h6>Deliberate falsification</h6>
+    <h4>About</h4>
+    <p>
+      Sensecraft promotes an ecosystem of <b>Trust</b> and <b>Safety</b> and
+      <b>Civil conversation</b>. To support that, disrespectful behavior,
+      including but not limited to threats, slander, libel, insults and
+      <i>ad hominem</i> attacks will be deemed unacceptable.
+    </p>
+    <p>
+      These are simple rules, a minimalist set. We expect to improve on them by
+      way of public Quests to gain deeper insight into the nature of game
+      governance.
+    </p>
   </q-page>
 </template>
-<script lang="ts"></script>
-<style lang="scss" scoped></style>
