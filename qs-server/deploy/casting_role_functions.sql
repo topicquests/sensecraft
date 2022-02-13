fdf1da3c72660c8dcac4b7ef3146fbdf8abf9a17
Marc-Antoine Parent <maparent@acm.org>
2022-02-13 09:51:31 -0500

    behave better when not logged in

--- qs-server/deploy/casting_role_functions.sql
+++ qs-server/deploy/casting_role_functions.sql
@@ -9,25 +9,36 @@ BEGIN;
 --
 
 CREATE OR REPLACE FUNCTION public.has_game_role(quest_id integer, guild_id integer, role_id integer) RETURNS boolean AS $$
-    SELECT count(*) > 0 FROM public.casting_role
+BEGIN
+  IF current_member_id() IS NULL THEN
+    RETURN false;
+  ELSE
+    RETURN (SELECT count(*) > 0 FROM public.casting_role
         WHERE
-          casting_role.role_id = role_id AND
+          casting_role.role_id = has_game_role.role_id AND
           casting_role.member_id = current_member_id() AND
-          casting_role.quest_id = quest_id AND
-          casting_role.guild_id = guild_id;
-$$ LANGUAGE sql STABLE;
+          casting_role.quest_id = has_game_role.quest_id AND
+          casting_role.guild_id = has_game_role.guild_id);
+  END IF;
+END;
+$$ LANGUAGE plpgsql STABLE;
 
 --
 -- Name: can_play_role(integer, integer); Type: FUNCTION
 --
 
 CREATE OR REPLACE FUNCTION public.can_play_role(guild_id integer, role_id integer) RETURNS boolean AS $$
-    SELECT count(*) > 0 FROM public.guild_member_available_role AS r
+BEGIN
+  IF current_member_id() IS NULL THEN
+    RETURN false;
+  ELSE
+    RETURN (SELECT count(*) > 0 FROM public.guild_member_available_role AS r
         WHERE
-          r.role_id = role_id AND
+          r.role_id = can_play_role.role_id AND
           r.member_id = current_member_id() AND
-          r.guild_id = guild_id;
-$$ LANGUAGE sql STABLE;
-
+          r.guild_id = can_play_role.guild_id);
+  END IF;
+END;
+$$ LANGUAGE plpgsql STABLE;
 
 COMMIT;
