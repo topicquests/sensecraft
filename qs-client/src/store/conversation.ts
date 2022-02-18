8b4eed7c7b04b5244cc705b628f193f6fe5cca38
Marc-Antoine Parent <maparent@acm.org>
2022-02-18 13:07:29 -0500

    corrections to scoring

--- qs-client/src/store/conversation.ts
+++ qs-client/src/store/conversation.ts
@@ -226,16 +226,16 @@ const ConversationGetters = {
     }
   },
   getGuildScoreMap: (state: ConversationState): ScoreMap => {
-    const scoreMap = MyVapi.store.getters["conversation/getScoreMap"];
+    const scoreMap = MyVapi.store.getters["conversation/getScoreMap"] || {};
     const guildScoreMap: ScoreMap = {};
     Object.keys(scoreMap).forEach((key) => {
       const guild_id = state.conversation[key].guild_id;
-      guildScoreMap.guild_id = (guildScoreMap.guild_id || 0) + scoreMap[key];
+      guildScoreMap[guild_id] = (guildScoreMap[guild_id] || 0) + scoreMap[key];
     });
     return guildScoreMap;
   },
   getPrivateGuildScoreMap: (state: ConversationState): ScoreMap => {
-    const scoreMap = MyVapi.store.getters["conversation/getPrivateScoreMap"];
+    const scoreMap = MyVapi.store.getters["conversation/getPrivateScoreMap"] || {};
     const guildScoreMap: ScoreMap = {};
     Object.keys(scoreMap).forEach((key) => {
       const guild_id = state.conversation[key].guild_id;
