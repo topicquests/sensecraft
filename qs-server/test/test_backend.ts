cfa042c4ca6eb4b2abe9ba1e57cdfeff56a2ae76
Marc-Antoine Parent <maparent@acm.org>
2022-07-30 13:13:46 -0400

    rename frontend.test so it is not part of mocha test. Make it clean up after a signal. Use ts-node to run it.

--- qs-server/test/test_backend.ts
+++ qs-server/test/test_backend.ts
@@ -1,7 +1,9 @@
-import { spawn, execSync } from 'child_process';
-import { axiosUtil } from './utils';
+import { spawn, execSync, ChildProcessWithoutNullStreams } from 'node:child_process';
+import process from 'node:process';
+import { axiosUtil, waitForListen } from './utils';
 
-let postgrest;
+let postgrest: ChildProcessWithoutNullStreams;
+let dispatcher: ChildProcessWithoutNullStreams;
 
 export const adminInfo = {
   name: 'Admin',
@@ -24,33 +26,32 @@ export const guildCreatorInfo = {
   email: 'guildcreator@example.com',
 };
 
-export async function mochaGlobalSetup() {
-  execSync('./scripts/db_updater.py -d test revert');
+
+async function frontendSetup() {
   execSync('./scripts/db_updater.py -d test init');
   execSync('./scripts/db_updater.py -d test deploy');
   postgrest = spawn('postgrest', ['postgrest_test.conf']);
-  let resolve;
-  const p = new Promise((rs) => {
-    resolve = rs;
-  });
-  function wakeup(data) {
-    data = data.toString();
-    console.log(data);
-    if (data.indexOf('Listening on port') > -1) {
-      setTimeout(resolve, 500);
-    }
-  }
-  postgrest.stderr.on('data', wakeup);
-  postgrest.stdout.on('data', wakeup);
-  await p;
+  dispatcher = spawn('node', ['dist/qs-server/dispatcher/main.js']);
+  await Promise.all([waitForListen(postgrest), waitForListen(dispatcher)]);
   await axiosUtil.call('create_member', adminInfo);
   execSync('python3 scripts/add_permissions.py -d test -u admin');
   await axiosUtil.call('create_member', questCreatorInfo);
   execSync('python3 scripts/add_permissions.py -d test -u questCreator -p createQuest');
   await axiosUtil.call('create_member', guildCreatorInfo);
   execSync('python3 scripts/add_permissions.py -d test -u guildCreator -p createGuild');
+  console.log('Ready');
 }
 
-exports.mochaGlobalTeardown = async function () {
-  console.log('something');
-};
+async function frontendTeardown () {
+  postgrest.kill('SIGINT');
+  dispatcher.kill('SIGINT');
+  execSync('./scripts/db_updater.py -d test revert');
+}
+
+process.on('SIGINT', async () => {
+  console.log('SIGINT');
+  await frontendTeardown();
+  console.log('done');
+});
+
+frontendSetup();
