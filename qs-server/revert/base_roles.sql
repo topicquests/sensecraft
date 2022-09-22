77d1f516901d4fa4f7dbe73bdb2d6c0ebf7aece5
Marc-Antoine Parent <maparent@acm.org>
2022-09-21 21:30:24 -0400

    role deletion is blocked by casting_roles

--- qs-server/revert/base_roles.sql
+++ qs-server/revert/base_roles.sql
@@ -3,6 +3,7 @@
 
 BEGIN;
 
+DELETE FROM public.casting_role WHERE role_id IN (SELECT id FROM public.role WHERE guild_id IS NULL AND name IN ('Guild leader', 'Game leader', 'Researcher', 'Scribe', 'Philosopher', 'Critic'));
 DELETE FROM public.role WHERE guild_id IS NULL AND name IN ('Guild leader', 'Game leader', 'Researcher', 'Scribe', 'Philosopher', 'Critic');
 DROP PROCEDURE IF EXISTS public.create_or_update_base_role(varchar, permission[], ibis_node_type[]);
 DROP PROCEDURE IF EXISTS public.create_or_update_base_role(varchar, permission[], public.publication_state, integer, JSON);
