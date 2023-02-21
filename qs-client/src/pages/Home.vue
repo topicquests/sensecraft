f2a913e39b5c4444474cbbccb85cbece8b7d3a1b
Byron Coughlin <byroncoughlinsr@gmail.com>
2023-02-21 06:13:03 -0800

    WIP home page

--- qs-client/src/pages/Home.vue
+++ qs-client/src/pages/Home.vue
@@ -2,45 +2,26 @@
   <q-page class="bg-secondary">
     <div class="row justify-center text-center">
       <h1 class="text-h1 q-pt-lg">SenseCraft</h1>
-      <h3 class="text-h3 q-pt-lg q-pb-lg">
+      <h3 class="text-h3 q-pt-lg">
         Where teams co-construct structured conversation
       </h3>
     </div>
-    <div class="row justify-center">
-      <img
-        class="q-pt-xl q-pb-lb"
-        src="../statics/earthrise2.png"
-        style="margin-left: auto; margin-right: auto"
-      />
-    </div>
-    <div class="row q-pb-sm q-pt-xl q-md-lg">
-      <div class="text-h5 text-bold q-px-sm q-pt-md">
-        Videos to improve your communication skills
-      </div>
-    </div>
-
-    <div class="row q-pt-xs">
-      <div class="col-12 col-md q-pa-md">
-        <q-card class="my-card">
-          <q-video
-            src="https://www.youtube.com/embed/WPF64UXFER0"
-            style="height: 300px"
-          />
-        </q-card>
+    <div class="wrapper">
+      <div class="signin-card">
+        <signinCard></signinCard>
       </div>
 
-      <div class="col-12 col-md q-pa-md">
-        <q-card class="my-card">
-          <q-video
-            src="https://www.youtube.com/embed/WPF64UXFER0"
-            style="height: 300px"
-          />
-        </q-card>
-      </div>
+      <div class="earth-image q-ma-md">
+        <img
+          src="../statics/earthrise2.png"
+          style="margin-left: auto; margin-right: auto"
+        />
+        <div class="text-h5 text-bold q-pt-md" style="margin-left: 23%">
+          Videos to improve your communication skills
+        </div>
 
-      <div class="col-12 col-md">
-        <div class="q-pa-md">
-          <q-card class="my-card">
+        <div class="youtube">
+          <q-card class="my-card" style="30%">
             <q-video
               src="https://www.youtube.com/embed/WPF64UXFER0"
               style="height: 300px"
@@ -49,27 +30,36 @@
         </div>
       </div>
     </div>
+
+    <div class="row justify-end" style="width: 100%">
+      <div class="col-4"></div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { mapActions } from "vuex";
 import { userLoaded } from "../boot/userLoaded";
+import SigninCard from "../components/signin-card.vue";
 
 export default {
   meta: {
-    title: "Home",
+    title: "Home"
   },
   name: "HomePage",
+  components: {
+    signinCard: SigninCard
+  },
+
   data() {
     return {
       ready: false,
-      payload: 100,
+      payload: 100
     };
   },
   methods: {
     ...mapActions("guilds", ["ensureAllGuilds"]),
-    ...mapActions("quests", ["ensureAllQuests"]),
+    ...mapActions("quests", ["ensureAllQuests"])
   },
 
   async beforeMount() {
@@ -80,12 +70,27 @@ export default {
     //   this.ensureAllGuilds(),
     // ])
     this.ready = true;
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .p1 {
   font: italic 20px Arial, sans-serif;
+}
+.wrapper {
+  display: flex;
+}
+.signin-card {
+  align-items: flex-end;
+}
+.earth-image {
+  align-self: flex-start;
+  align-items: center;
+}
+.youtube {
+  width: 50%;
+  height: 50%;
+  margin-left: 30%;
 }
 </style>
