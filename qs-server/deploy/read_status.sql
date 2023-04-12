0783c3ccb251192ea43af53d9993d28bf0ac450f
Marc-Antoine Parent <maparent@acm.org>
2023-04-12 11:13:13 -0400

    formatting

--- qs-server/deploy/read_status.sql
+++ qs-server/deploy/read_status.sql
@@ -1,29 +1,21 @@
 -- Deploy sensecraft:read_status to pg
-
--- requires members
-
 -- requers conversation_node
 
 BEGIN;
 
 --
-
 -- Name: read_status; Type: TABLE
-
 --
 
-CREATE TABLE
-    IF NOT EXISTS public.read_status (
-        node_id INTEGER NOT NULL,
-        member_id INTEGER NOT NULL,
-        last_read TIMESTAMP
-        WITH
-            TIME ZONE,
-            seconds_shown INTERVAL,
-            force_unread BOOLEAN DEFAULT FALSE,
-            PRIMARY KEY (node_id, member_id),
-            CONSTRAINT read_status_node_id_fkey FOREIGN KEY (node_id) REFERENCES public.conversation_node(id) ON UPDATE CASCADE ON DELETE CASCADE,
-            CONSTRAINT read_status_member_id_fkey FOREIGN KEY (member_id) REFERENCES public.members(id) ON UPDATE CASCADE ON DELETE CASCADE
-    );
+CREATE TABLE IF NOT EXISTS public.read_status (
+    node_id INTEGER NOT NULL,
+    member_id INTEGER NOT NULL,
+    last_read TIMESTAMP WITH TIME ZONE,
+    seconds_shown INTERVAL,
+    force_unread BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (node_id, member_id),
+    CONSTRAINT read_status_node_id_fkey FOREIGN KEY (node_id) REFERENCES public.conversation_node(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT read_status_member_id_fkey FOREIGN KEY (member_id) REFERENCES public.members(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 COMMIT;
