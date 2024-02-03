767d88e412f54fbe3947d415a9e9776145f40068
Marc-Antoine Parent <maparent@acm.org>
2024-02-03 07:12:33 -0500

    repair crash

--- sc-client/src/pages/QuestList.vue
+++ sc-client/src/pages/QuestList.vue
@@ -3,7 +3,7 @@
     <div class="row justify-center">
       <q-card class="q-mt-md quest-card">
         <div>
-          <member></member>
+          <!-- member></member -->
         </div>
         <div class="column items-center">
           <div class="col-12 q-mb-md">
@@ -47,14 +47,14 @@
 </template>
 
 <script setup lang="ts">
-import scoreboard from '../components/scoreboard.vue';
+import scoreboard from '../components/score-board.vue';
 import questTable from '../components/quest-table.vue';
-import member from '../components/member.vue';
+//import member from '../components/member.vue';
 
 import { userLoaded } from '../boot/userLoaded';
 import { useMemberStore } from 'src/stores/member';
 import { useQuestStore } from 'src/stores/quests';
-import { useGuildStore } from 'src/stores/guilds'
+import { useGuildStore } from 'src/stores/guilds';
 import { onBeforeMount } from 'vue';
 
 const memberStore = useMemberStore();
@@ -63,18 +63,16 @@ const guildStore = useGuildStore();
 
 let ready = false;
 
-  isAuthenticated: false;
-  serverData: [];
-
-   onBeforeMount(async () => {
-    await userLoaded;
-    // not using those yet?
-    await Promise.all([
-      questStore.ensureAllQuests(),
-      // guildStore.setCurrentGuild(false),
-      // questStore.setCurrentQuest(true),
-    ]);
-    ready = true;
+onBeforeMount(async () => {
+  await userLoaded;
+  // not using those yet?
+  await Promise.all([
+    questStore.ensureAllQuests(),
+    // guildStore.setCurrentGuild(false),
+    // questStore.setCurrentQuest(true),
+  ]);
+  ready = true;
+});
 </script>
 
 <style>
