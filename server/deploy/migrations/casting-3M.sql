3b048e02c07b8504d64ccd2295b04c14015d4267
Marc-Antoine Parent <maparent@acm.org>
2026-03-14 12:26:32 -0400

    repair migration

--- server/deploy/migrations/casting-3M.sql
+++ server/deploy/migrations/casting-3M.sql
@@ -3,13 +3,17 @@
 
 BEGIN;
 
+-- following operation needs disabling trigger
+DROP TRIGGER IF EXISTS before_update_casting ON public.casting;
 -- Update all existing casting records to include createRoleChannel permission
-UPDATE public.casting 
+UPDATE public.casting
 SET permissions = array_append(permissions, 'createRoleChannel'::public.permission)
 WHERE NOT ('createRoleChannel'::public.permission = ANY(permissions));
+-- reinstate trigger
+CREATE TRIGGER before_update_casting BEFORE UPDATE ON public.casting FOR EACH ROW EXECUTE FUNCTION public.before_update_casting();
 
 -- Change the default value for new records
-ALTER TABLE public.casting 
+ALTER TABLE public.casting
 ALTER COLUMN permissions SET DEFAULT ARRAY['createRoleChannel']::public.permission[];
 
 COMMIT;
