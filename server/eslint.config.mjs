4da3b3d8ec7c4d6251545d80c0caeb033a606168
Byron Coughlin <byroncoughlinsr@gmail.com>
2026-04-16 15:58:46 -0700

    updated package.json

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -18,12 +18,10 @@ const compat = new FlatCompat({
 export default [...compat.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:mocha/recommended",
     "prettier",
-), {
+), mocha.configs.flat.recommended, {
     plugins: {
         "@typescript-eslint": typescriptEslint,
-        mocha,
     },
 
     languageOptions: {
